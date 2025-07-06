f177352ac557ce69e1f73c98bab8105b2dbb2892
Natanael Gomes Cardoso <natangomescardoso0@gmail.com>
2025-07-06 16:18:54 -0300

    Adiciona layout base com Navbar, Footer e rotas iniciais

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
+
+@app.context_processor
+def inject_now():
+    return {'now': datetime.utcnow}
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+# rotas vazias por enquanto
+@app.route('/produtos')
+def cadastro_produtos():
+    return render_template('coming_soon.html', módulo='Cadastro de Produtos')
+
+@app.route('/clientes')
+def cadastro_clientes():
+    return render_template('coming_soon.html', módulo='Cadastro de Clientes')
+
+@app.route('/os')
+def os_listar():
+    return render_template('coming_soon.html', módulo='Listagem de OS')
 
 if __name__ == '__main__':
     app.run(debug=True)
